83237cecaf914e3bc95a3069098e30adb5b5f1f9
Naitik Shah <n@daaku.org>
2016-04-10 12:21:40 -0700

    temporary change to use my rocksdb changes

--- embedflag.go
+++ embedflag.go
@@ -4,7 +4,7 @@ package gorocksdb
 
 // #cgo CXXFLAGS: -std=c++11
 // #cgo CPPFLAGS: -I${SRCDIR}/../../cockroachdb/c-lz4/internal/lib
-// #cgo CPPFLAGS: -I${SRCDIR}/../../cockroachdb/c-rocksdb/internal/include
+// #cgo CPPFLAGS: -I${SRCDIR}/../../daaku/c-rocksdb/internal/include
 // #cgo CPPFLAGS: -I${SRCDIR}/../../cockroachdb/c-snappy/internal
 // #cgo LDFLAGS: -lstdc++
 // #cgo darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
@@ -13,6 +13,6 @@ import "C"
 
 import (
 	_ "github.com/cockroachdb/c-lz4"
-	_ "github.com/cockroachdb/c-rocksdb"
 	_ "github.com/cockroachdb/c-snappy"
+	_ "github.com/daaku/c-rocksdb"
 )
